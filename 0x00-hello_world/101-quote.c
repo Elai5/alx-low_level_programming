b3b359d5950c6e2873e74eeff56692293501e83f
Elai5 <yvettelaine007@gmail.com>
2023-02-16 05:37:34 -0800

    pulop

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -  a new program with asciii both std
+ * Return 1 (wow)
+*/
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
