5b518baa85c66248f4d92f5e25416046183211d5
Elai5 <yvettelaine007@gmail.com>
2023-02-16 04:20:00 -0800

    newline

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the second task
+ * Return: 0 (donedeal)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+		return (0);
+}
