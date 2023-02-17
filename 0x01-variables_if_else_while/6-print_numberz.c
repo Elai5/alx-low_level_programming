6edb6233eb4de871cbd5c5c6594b81b230429588
Elai5 <yvettelaine007@gmail.com>
2023-02-16 23:38:21 -0800

    numberc

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - numberz c lower
+ * Return: 0 (sucnum)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
