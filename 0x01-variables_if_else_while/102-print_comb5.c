103e7760517c136b98d7fb0a71832774137aba0f
Elai5 <yvettelaine007@gmail.com>
2023-02-17 00:38:08 -0800

    sdevelop

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - software development is daang
+*/
+int main(void)
+{
+	int i = 0;
+	int j;
+	int count = 0;
+
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (count != 4949)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+			count++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
