dd718ab07206ffffb3c0c42c8824b4ead91dbd35
Elai5 <yvettelaine007@gmail.com>
2023-02-16 23:34:45 -0800

    new begining

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a number of lowercase number
+ * Return: zero (try)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
