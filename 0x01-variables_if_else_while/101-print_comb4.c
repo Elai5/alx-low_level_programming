ac90ad599b2c100bc7838c64a1770b0fa1584d11
Elai5 <yvettelaine007@gmail.com>
2023-02-17 00:35:00 -0800

    three difrent digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - diffrent combination of 3 digits
+ * Return:0
+*/
+int main(void)
+{
+	int i = 0;
+	int j;
+	int count = 0;
+
+	while (i <= 8)
+	{
+		j = i + 1;
+		while (j <= 9)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (count != 44)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+			count++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
