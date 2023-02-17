a1a442a845dc651c9da16673afbcd0e250b4aa62
Elai5 <yvettelaine007@gmail.com>
2023-02-16 22:59:15 -0800

    number pos

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Check if a number.
+ * @void: Null value
+ *
+ * Description: Print positive, negative or 0 number
+ * Return: Zero value
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
