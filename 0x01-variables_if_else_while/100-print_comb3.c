5aeffef4cd0a8b122ff53529d25ba1c1372e44df
Elai5 <yvettelaine007@gmail.com>
2023-02-17 00:32:13 -0800

    two diffrent digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - diffrent combination of two diggits"
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 99; n++)
+	{
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
+		if (n != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
